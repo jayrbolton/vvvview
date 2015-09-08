8f73277e07cc9c2a2864654cda446aff0c506fd2
Jay R Bolton <jay@commitchange.com>
2015-09-07 23:35:38 -0700

    Update the todo example to use the new api

---
+++ examples/todo/index.es6.js
@@ -0,0 +1,49 @@
+'use strict'
+import {flyd, combineState, createView} from '../../index.es6.js'
+import {vtree, toggleItem, submitForm, sUndos} from './vtree.js'
+
+// See ./vtree.js for the virtual-dom components
+
+//// view creation and event/state streams
+
+// use localStorage, why not?
+let cache = localStorage.getItem('todos')
+let initialState = cache ? JSON.parse(cache) : {items: [{name: 'Do the dishes!'}]}
+
+// init the view
+let todos = createView(document.body, vtree, initialState)
+
+// sync state to localStorage. could probably use frp for this instead
+todos.sync = function(state) {
+  localStorage.setItem('todos', JSON.stringify(state))
+}
+
+
+// add a new item
+let newItem = flyd.map((ev) => {
+  ev.preventDefault()
+  let item = { name: ev.target.querySelector('input').value }
+  ev.target.reset()
+  return item
+}, submitForm)
+
+
+combineState((state, item) => {
+  state.items.push(item)
+  return state
+}, todos, [newItem])
+
+
+// toggle state of existing item
+let toggle = flyd.map((params) => {
+ let [ev, items, index] = params
+ items[index].finished = !items[index].finished
+ return items
+}, toggleItem)
+
+
+combineState((state, items) => {
+  state.items = items
+  return state
+}, todos, [toggle])
+
---
+++ examples/todo/vtree.js
@@ -0,0 +1,41 @@
+import {mori, partial, h, flyd} from '../../index.es6.js'
+window.flyd = flyd
+
+let toggleItem = flyd.stream()
+let toggleItemScoped = (items, index) => ev => toggleItem([ev, items, index])
+let submitForm = flyd.stream()
+
+const root = state => {
+  let finishedLen = state.items.filter(item => item.finished).length // could cache this better
+  return h('div', [
+    partial(itemForm),
+    h('p', [finishedLen, ' out of ', state.items.length, ' finished']),
+    partial(itemList, state.items)
+  ])
+}
+
+const itemList = items =>
+  items.length ?
+  h('ul', items.map((item, index) => partial(itemRow, items, index))) :
+  h('p', 'Your slate is clean!')
+
+const itemForm = () =>
+  h('form', {onsubmit: submitForm}, [
+    h('input', {required: true, type: 'text', placeholder: 'What needs to be done?'}),
+    h('button', {type: 'submit'}, 'Add item')
+  ])
+
+const itemRow = (items, index) => {
+  let item = items[index]
+  return h('li', [
+    h('input', {type: 'checkbox', checked: item.finished, onchange: toggleItemScoped(items, index)}),
+    h('span', {className: item.finished && 'is-finished'}, item.name)
+  ])
+}
+
+module.exports = {
+  vtree: root,
+  submitForm: submitForm,
+  toggleItem: toggleItem,
+}
+

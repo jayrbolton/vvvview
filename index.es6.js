c358a67ad5ff12a260426dfac04ba4b0e6008d61
Jay R Bolton <jay@commitchange.com>
2015-09-07 23:36:07 -0700

    Various bugfixes and improvements. Breaking api change.

---
+++ index.es6.js
@@ -0,0 +1,43 @@
+import createElement from 'virtual-dom/create-element'
+import h from 'virtual-dom/h'
+import patch from 'virtual-dom/patch'
+import diff from 'virtual-dom/diff'
+import flyd from 'flyd'
+import createPartial from './thunk'
+import {curryN} from 'fj-curry'
+
+var api = { h: h, flyd: flyd, partial: createPartial }
+
+module.exports = api
+
+// Create an object literal that contains all the data for view re-rendering and a state stream
+api.createView = curryN(3, (parentNode, rootComponent, initialState) => {
+  var view = { rootComponent: rootComponent, streams: {}, state: initialState}
+  view.tree = rootComponent(view.state)
+  view.rootNode = createElement(view.tree)
+  parentNode.appendChild(view.rootNode)
+  return view
+})
+
+// Given some streams, combine it into a view's state stream using a combinator.
+// This allows you to compose any number of arbitrary streams into the view's
+// main state stream so that the dom gets rerendered automatically.
+api.combineState = curryN(3, (combinator, view, stream) => {
+  if(stream.length) stream = stream.reduce((acc, s) => flyd.merge(acc, s))
+  return flyd.stream([stream], (n, changes) => {
+    view.state = combinator.apply(null, [view.state, stream()])
+    render(view, view.state)
+    return view.state
+  })
+})
+
+// Render a virtual dom tree given a view and a state, returns the state
+const render = curryN(2, (view, state) => {
+  if(view.sync) view.sync(state)
+  var newTree = view.rootComponent(state)
+  var patches = diff(view.tree, newTree)
+  view.rootNode = patch(view.rootNode, patches)
+  view.tree = newTree
+  return state
+})
+

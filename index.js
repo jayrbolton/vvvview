7078f185f8622a3ea685f5636c331c30b7de5aa0
Jay R Bolton <jayrbolton@gmail.com>
2016-01-27 11:20:32 -0800

    Clean up everything

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-"use strict"
-import createElement from 'virtual-dom/create-element'
-import patch from 'virtual-dom/patch'
-import diff from 'virtual-dom/diff'
-import {curryN} from 'fj-curry'
+var createElement = require('virtual-dom/create-element')
+var patch = require('virtual-dom/pach')
+var diff = require('virtual-dom/diff')
+var curryN = require('fj-curry').curryN
 
-const view = curryN(3, (rootFn, parentNode, state) => {
+var view = curryN(3, function(rootFn, parentNode, state) {
  let view = {root: rootFn, tree: rootFn(state)}
  view.rootNode = createElement(view.tree)
  parentNode.appendChild(view.rootNode)
  return rerender(view)
 })
 
-const rerender = curryN(2, (view, newState) => {
+vas rerender = curryN(2, function(view, newState) {
  let newTree = view.root(newState)
  let patches = diff(view.tree, newTree)
  view.rootNode = patch(view.rootNode, patches)
